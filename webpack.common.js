62ee5bb0a25447f90223bcc9253485fc51cb2ed5
weeks6 <bridcarp1@gmail.com>
2021-12-08 22:06:23 +0300

    temp perm webpack fix

--- webpack.common.js
+++ webpack.common.js
@@ -1,34 +1,34 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/js/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                    presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.html$/,
-                use: ['html-loader']
-            },
-            {
-                test: /\.(png|svg|jpg)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[hash].[ext]',
-                        outputPath: "assets",
-                        esModule: false
-                    }
-                }
-            }
-        ]
-    }
-}
+  entry: './src/js/index.js',
+  module: {
+    rules: [
+      // {
+      //     test: /\.js$/,
+      //     exclude: /(node_modules)/,
+      //     use: {
+      //         loader: 'babel-loader',
+      //         options: {
+      //         presets: ['@babel/preset-env']
+      //         }
+      //     }
+      // },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'assets',
+            esModule: false,
+          },
+        },
+      },
+    ],
+  },
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -8,38 +8,35 @@ const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
-    mode: 'production',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.[contentHash].js'
-    },
-    optimization: {
-        minimizer: [
-            new OptimizeCssAssetsPlugin(),
-            new TerserPlugin()
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contentHash].css'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            favicon: './src/assets/mobile-logo.svg',
-            minify: {
-                removeAttributeQuotes: true,
-                collapseWhitespace: true,
-                removeComments: true
-            }
-        })
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contentHash].js',
+  },
+  optimization: {
+    minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contentHash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './src/assets/mobile-logo.svg',
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-            },
-        ]
-    }
+  },
 });
